51c00b1140ee2d6301204350772dd9742855aafe
Crist Missa <47691426+Pretty-M@users.noreply.github.com>
2023-09-27 10:35:24 +0800

    Add files via upload
    lenet model train

---
+++ Image_Classification/Model/LeNet/LeNet.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+print(torch.cuda.is_available())
+
+
+# *************************** LeNet-5模型 ********************************* #
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):  # x shape: (batch, 3, 32, 32)
+        x = F.relu(self.conv1(x))  # shape: (batch, 16, 28, 28)
+        x = self.pool1(x)  # shape: (batch, 16, 14, 14)
+        x = F.relu(self.conv2(x))  # shape: (batch, 32, 10, 10)
+        x = self.pool2(x)  # shape: (batch, 32, 5, 5)
+        x = x.view(-1, 32 * 5 * 5)  # shape: (batch, 32 * 5 * 5)
+        x = F.relu(self.fc1(x))  # shape: (batch, 120)
+        x = F.relu(self.fc2(x))  # shape: (batch, 84)
+        x = self.fc3(x)  # shape: (batch, 10)
+        return x
